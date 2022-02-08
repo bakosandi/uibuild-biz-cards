682b3ea25e6b082b7c2c78eaf17fed319190ee51
bakosandi <bakso89@gmail.com>
2022-02-08 11:55:40 +0100

    add new updates

---
+++ sitebuild/src/components/Landing.jsx
@@ -0,0 +1,32 @@
+const Landing = () => {
+  return (
+    <div className="container">
+      <div className="card1">
+        <h2>
+          John <br /> Freeman
+        </h2>
+        <h6>Pizza Thief</h6>
+        <p>2610 NW Vaughn St, Portland,</p>
+        <p>OR 97210, United States</p>
+      </div>
+      <div className="card2">
+        <h2>
+          Angela <br /> Slider
+        </h2>
+        <h6>Max Steaks</h6>
+        <p>3621 Germany Bunderlinger Strasse 345</p>
+        <p>PA 19543, United States</p>
+      </div>
+      <div className="card3">
+        <h2>
+          Rita <br /> Lehigh
+        </h2>
+        <h6>Pizza Ranch</h6>
+        <p>1234 Hungarain Road Closure</p>
+        <p>MT 12345, United States</p>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
